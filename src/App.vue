5a2487d5989df85897d9c8cd85fcd7342e8b6df4
Simon Nyblom <simnyb01@easv365.dk>
2025-03-16 21:55:09 +0100

    start of project

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
-<script setup>
-</script>
-
 <template>
   <div>
-
-
+    <button>{{ content.button.buttonSignUp }}</button>
+    <header>
+      <h1>{{ content.header.siteTitle }}</h1>
+      <nav>
+        <ul>
+          <li>{{ content.header.navigationHome }}</li>
+          <li>{{ content.header.navigationAbout }}</li>
+        </ul>
+      </nav>
+    </header>
   </div>
 </template>
 
-<style scoped>
+<script setup>
+import content from './assets/content.json';
+</script>
 
+<style scoped>
+/* Your styles here */
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <button>{{ content.button.buttonSignUp }}</button>
+      <header>
+        <h1>{{ content.header.siteTitle }}</h1>
+        <nav>
+          <ul>
+            <li>{{ content.header.navigationHome }}</li>
+            <li>{{ content.header.navigationAbout }}</li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  </template>
+  
+  <script>
+  import content from '@/assets/content.json';
+  
+  export default {
+    data() {
+      return {
+        content
+      };
+    }
+  };
+  </script>
+  
