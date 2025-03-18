bdc4b9910a07f815659dfd739cc76dcd635e71aa
Simon Nyblom <simnyb01@easv365.dk>
2025-03-18 11:18:49 +0100

    made json import work

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
     <div>
-      <h1>Welcome to My Portfolio</h1>
+      <h1>{{ content.welcome.title }}</h1>
+      <h2>{{ content.welcome.description }}</h2>
     </div>
   </template>
+
+<script setup>
+import content from '../assets/content.json';
+</script>
+
+<style lang="scss" scoped>
+</style>
+
   
